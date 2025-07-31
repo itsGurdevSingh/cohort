0c855b637d4ff58bd7e53b475f5c97d261d636cf
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2025-07-31 21:34:31 +0530

    folder stucture updated in backend

--- 4.backend/expressServer/ExpressServer.js
+++ 4.backend/expressServer/ExpressServer.js
@@ -4,6 +4,6 @@ const app = express()
 
 const port = 3000;
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log(`server is running on port ${port}`)
 })
--- 4.backend/httpServer/httpServer.js
+++ 4.backend/httpServer/httpServer.js
@@ -6,6 +6,6 @@ const server = http.createServer((req,res)=>{
 
 const port = 3000;
 
-server.listen(3000,()=>{
+server.listen(port,()=>{
     console.log(`server is running on port ${port}`)
 })
