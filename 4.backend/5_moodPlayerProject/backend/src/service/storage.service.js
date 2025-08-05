b0323699419f7ac3ec31e4710ec8a508006548ee
gurdevsinghvip@gmail.com <gurdevsinghvip@gmail.com>
2025-08-05 22:10:26 +0530

    imagekit key fixed

--- 4.backend/5_moodPlayerProject/backend/src/service/storage.service.js
+++ 4.backend/5_moodPlayerProject/backend/src/service/storage.service.js
@@ -3,12 +3,13 @@ const ImageKit = require("imagekit");
 require("dotenv").config();
 
 const imagekit = new ImageKit(
-            {publicKey: process.env.IMAGEKIT_PUBLIC_KEY +'=',
-            privateKey: process.env.IMAGEKIT_PRIVATE_KEY +'=',
+            {publicKey: process.env.IMAGEKIT_PUBLIC_KEY ,
+            privateKey: process.env.IMAGEKIT_PRIVATE_KEY ,
             urlEndpoint: process.env.IMAGEKIT_URL_END_POINT
             });
 
 const uploadToImagekit = (file) => {
+
     return new Promise((resolve, reject) => {
         imagekit.upload({
             file: file.buffer,
